ce76751c25372ce8e30cac9a6d6e2e8023f1722c
Jason Zheng <zzheng3@stuy.edu>
2018-11-08 00:32:28 -0500

    finished with addWordHorizontal

--- WordSearch.java
+++ WordSearch.java
@@ -25,20 +25,16 @@ public class WordSearch{
       }
       return board;
     }
-
-
-    /**Attempts to add a given word to the specified position of the WordGrid.
-     *The word is added from left to right, must fit on the WordGrid, and must
-     *have a corresponding letter to match any letters that it overlaps.
-     *
-     *@param word is any text to be added to the word grid.
-     *@param row is the vertical locaiton of where you want the word to start.
-     *@param col is the horizontal location of where you want the word to start.
-     *@return true when the word is added successfully. When the word doesn't fit,
-     * or there are overlapping letters that do not match, then false is returned
-     * and the board is NOT modified.
-     */
     public boolean addWordHorizontal(String word,int row, int col){
+      if (word.length() > data[row].length - col) {
+        return false;
+      }
+      int index = 0;
+      for(int i = col; i < word.length() + col; i = i + 1) {
+        data[row][i] = word.charAt(index);
+        index = index + 1;
+      }
+      return true;
     }
 
    /**Attempts to add a given word to the specified position of the WordGrid.
